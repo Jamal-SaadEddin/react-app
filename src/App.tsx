2ad0cb3b03a4157259ad694918ff9a9c0290932e
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-04-30 06:53:02 +0300

    2- Creating a ListGroup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;

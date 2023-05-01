a2d500094135e4c970356391f9c1b89e55b2845b
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-01 06:48:02 +0300

    5- Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>

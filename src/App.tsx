574b171429b57ebefc4ddf764c7c8c3f1f9a62d2
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-02 09:56:42 +0300

    2- Vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)} color="primary">
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
+import "./ListGroup.css";
 
 interface Props {
   items: string[];

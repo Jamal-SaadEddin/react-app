0681cd726837568b1b5c1f25a654fd95157111ce
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-11 14:23:14 +0300

    7- Updating Nested Objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import Like from "./components/Like";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
   });
 
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
   };
 
   return (
     <div>
-      {drink.price}
+      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );

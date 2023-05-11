1c3d090b3aade351b9bc673e7e34ae9cd8bff90c
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-11 15:03:51 +0300

    10- Simplifying Update Logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
-import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: "notFixed" },
-    { id: 2, title: "Bug 2", fixed: "notFixed" },
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
   ]);
 
   const handleClick = () => {
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
     setBugs(
-      bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: "Fixed" } : bug))
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
     );
   };
 
   return (
     <div>
-      {bugs[0].fixed}
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );

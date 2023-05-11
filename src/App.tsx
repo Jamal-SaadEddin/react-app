b4f71e094b0bccba280cb4726df301c412cda61e
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-11 14:14:07 +0300

    6- Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Like from "./components/Like";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
   const handleClick = () => {
-    console.log("Clicked");
+    setDrink({ ...drink, price: 6 });
   };
 
   return (
     <div>
-      <Like onClick={handleClick} />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }

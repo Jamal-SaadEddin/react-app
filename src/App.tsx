89927ebc7ca0734114ac327937622a419747d27d
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-13 16:46:46 +0300

    2- Building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quam
-        fugiat? Laudantium earum doloribus, veniam ducimus ratione possimus
-        dignissimos ad accusamus exercitationem iure similique atque laboriosam
-        facilis minus. At iste quisquam excepturi harum facilis expedita itaque
-        ipsum illo rem molestiae voluptatibus reiciendis modi quod nulla
-        explicabo, ex ab quos. Porro vero culpa consectetur ut! Laborum dolorem
-        et iure nam architecto, accusantium suscipit minus. Ipsa quam
-        praesentium distinctio saepe quasi eveniet modi nam aut nostrum sapiente
-        nisi accusantium cum, quos explicabo, sequi aspernatur. Distinctio, eos?
-        In eos, iure impedit culpa praesentium maiores ratione, cumque quidem ad
-        alias magnam inventore, ex aut?
-      </ExpandableText>
+      <Form />
     </div>
   );
 }

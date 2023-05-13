56b21953eb19c62fb5ab542eeb924e82375c5b21
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-13 13:17:18 +0300

    12- Exercise 3- Updating State

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import { useState } from "react";
 import produce from "immer";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza(
-      produce((pizza) => {
-        pizza.toppings.push("Cheese");
-      })
-    );
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      <p>{pizza.toppings.join(", ")}</p>
-      <button onClick={handleClick}>Add topping</button>
+      {cart.items.map((item) => (
+        <p key={item.id}>
+          {item.title} - Quantity: {item.quantity}
+        </p>
+      ))}
+      <button onClick={handleClick}>Change quantity</button>
     </div>
   );
 }

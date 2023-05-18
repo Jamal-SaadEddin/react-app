8ef6ec2aa9ab690d59b404856b83572fd377b108
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-05-18 12:29:25 +0300

    Revert "4- Accessing Input Fields"
    This reverts commit c1edd79bd87c87462782293ecc68dbc88a2ebfe8.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,9 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
-    name: "",
-    age: 0,
-  };
-
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-    console.log(person);
+    console.log("Submitted");
   };
 
   return (
@@ -21,13 +12,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit

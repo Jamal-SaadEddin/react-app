a6938cfea7bf536a88dabb7b6133fda7f7e5df92
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-02 16:14:36 +0300

    Revert "Project- Expense Tracker - My Solution (Jamal)"
    This reverts commit f14aa90a1d369dd31337b2c58b928045cc80780a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,9 @@
-import { useState } from "react";
-import ExpenseFilter from "./components/ExpenseFilter";
-import ExpenseList from "./components/ExpenseList";
+import Form from "./components/Form";
 
 function App() {
-  const [expenseItems, setExpenseItems] = useState([
-    {
-      id: 1,
-      description: "Milk",
-      amount: 5,
-      category: "Groceries",
-    },
-    {
-      id: 2,
-      description: "Eggs",
-      amount: 10,
-      category: "Groceries",
-    },
-    {
-      id: 3,
-      description: "Electricity",
-      amount: 100,
-      category: "Utilities",
-    },
-    {
-      id: 4,
-      description: "Movies",
-      amount: 15,
-      category: "Entertainment",
-    },
-    {
-      id: 5,
-      description: "Milk",
-      amount: 5,
-      category: "Groceries",
-    },
-  ]);
-
   return (
     <div>
-      <ExpenseList
-        expenseItems={expenseItems}
-        setExpenseItems={setExpenseItems}
-      />
-      <ExpenseFilter
-        expenseItems={expenseItems}
-        setExpenseItems={setExpenseItems}
-      />
+      <Form />
     </div>
   );
 }
--- src/servicies/filterCategory.tsx
+++
@@ -1,6 +0,0 @@
-function filterByCategory(expenseItems: object[], category: string) {
-  if (category === "All categories") return expenseItems;
-  return expenseItems.filter((item) => item.category === category);
-}
-
-export default filterByCategory;

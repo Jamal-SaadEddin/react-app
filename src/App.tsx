a9749a6deffd005ba0e0b4f06d52d527e7855116
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-02 16:32:37 +0300

    11- Building ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  if (expenses.length === 0) return null;
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }

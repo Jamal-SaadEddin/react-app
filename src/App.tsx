565363b8708c5012972c9d376e2e200f60a3da8c
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-02 17:16:15 +0300

    12- Building ExpenseFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
@@ -9,12 +12,19 @@ function App() {
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
-  if (expenses.length === 0) return null;
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All categories</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;

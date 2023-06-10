fef5a16e54000b764a88912f653078ac62852766
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-10 21:01:10 +0300

    3- Effect Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
+  const [category, setCategory] = useState("");
 
   // afterRender
-  useEffect(() => {
-    // Side effect
-    if (ref.current) ref.current.focus();
-  });
-
-  useEffect(() => {
-    document.title = "My App";
-  });
+  useEffect(() => {});
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }

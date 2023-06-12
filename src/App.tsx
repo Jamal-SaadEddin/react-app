378d5da0d7b445456201582a45aff2f1338bce88
Jamal SaadEddin <599098598@asaledu.onmicrosoft.com>
2023-06-12 14:40:10 +0300

    9- Cancelling a Fetch Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -11,23 +11,19 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/1users"
-        );
-        setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+    const controller = new AbortController();
 
-    fetchUsers();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
-    // axios
-    //   .get<User[]>("https://jsonplaceholder.typicode.com/1users")
-    //   .then((res) => setUsers(res.data))
-    //   .catch((err) => setError(err.message));
+    return () => controller.abort();
   }, []);
 
   return (
